0a000df38d8e8d2d440a665caf8cff570b42d7c5
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-08-03 23:19:36 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # バリデーション
+  validates :name,  presence: true
+  validates :email, presence: true, uniqueness: true
+
+  # デバイス
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/edit'
+  get 'users/show'
   root 'homes#index'
 
   namespace :admin do
-    devise_for :users
+    resources :users
     root 'homes#index'
   end
 end
