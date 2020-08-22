7163842b057b47d4e8674c695dd28cc4cd9de851
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-08-22 13:00:16 +0900

    商品新規登録、編集

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,4 +5,40 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.order(created_at: :desc)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: "商品「#{@product.title}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: "商品「#{@product.title}」を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :price, :introduction, :user_id, :category_id)
+  end
 end
