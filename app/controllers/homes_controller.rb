f4ce30ef9b3e4d001dbd1065379dd2af6a08ad96
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-10-24 15:42:27 +0900

    top商品表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,5 +2,7 @@ class HomesController < ApplicationController
   def index
     @products = Product.all
     @categories = Category.all
+    @samples = @products.sample(6)
+    @new_products = @products.order(created_at: :desc).first(6)
   end
 end
