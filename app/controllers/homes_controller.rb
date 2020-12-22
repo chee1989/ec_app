f9cab04691ea3666c45f1e23664bcbae2df590ef
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-12-22 23:33:17 +0900

    トップページにいいねの多い順追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,10 +4,11 @@ class HomesController < ApplicationController
     @categories = Category.includes(:products)
     @samples = @products.where('title LIKE ?', "%テスト%").page(params[:page]).per(3)
     @new_products = @products.order(created_at: :desc).page(params[:page]).per(3)
+    @like_products = Product.with_attached_images.includes(:user, :category).like_sort_desc.page(params[:page]).per(3)
 
     return unless request.xhr?
     case params[:type]
-    when 'sample', 'new_pro'
+    when 'sample', 'new_pro', 'like_pro'
       render "#{params[:type]}"
     end
   end
