b01f9edd4b885323a45e222f357d699b77c210c1
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-12-23 22:34:33 +0900

    UI調整

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,12 @@ class LikesController < ApplicationController
 
   def create
     current_user.likes.create(product_id: params[:product_id])
-    redirect_to product_path(params[:product_id])
+    @product = Product.find(params[:product_id])
   end
 
   def destroy
     @product = Product.find(params[:id])
     @like = Like.find_by(product_id: @product.id, user_id: current_user.id)
     @like.destroy
-    redirect_to product_path(@product)
   end
 end
