b37a664219f667f23fb3a32a140c62e4563b4e4d
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-12-26 20:39:26 +0900

    controllerテスト

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe ProductsController, type: :controller do
-  before do
-    allow(controller).to receive(:authenticate_user!).and_return(true)
-  end
+RSpec.describe HomesController, type: :controller do
   describe "#index" do
-    # 正常にレスポンスを返すこと
+    # 正常にレスポンスを返すこと 
     it "responds successfully" do
       get :index
       expect(response).to be_success
@@ -14,6 +11,6 @@ RSpec.describe ProductsController, type: :controller do
     it "returns a 200 response" do
       get :index
       expect(response).to have_http_status "200"
-    end
+     end
   end
 end
