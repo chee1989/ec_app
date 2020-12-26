af2563d550b682c862c4d6cd22aa64e7972ab546
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-12-26 19:25:48 +0900

    modelテスト

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @product = FactoryBot.create(:product)
+  end
+  # ユーザーID、商品IDがあれば有効であること
+  it "is valid with a user_id and product_id" do
+    favo = Favorite.new(
+      user_id: @user.id,
+      product_id: @product.id,
+    )
+    expect(favo).to be_valid
+  end
+  # ユーザーIDがなければ無効な状態であること
+  it "is invalid without a user_id" do
+    favo = Favorite.new(user_id: nil)
+    favo.valid?
+    expect(favo.errors[:user]).to include("を入力してください")
+  end
+  # 商品IDがなければ無効な状態であること
+  it "is invalid without a product_id" do
+    favo = Favorite.new(product_id: nil)
+    favo.valid?
+    expect(favo.errors[:product]).to include("を入力してください")
+  end
+  # 重複したいいねは無効な状態であること
+  it "is invalid with a duplicate favorites" do
+    FactoryBot.create(:favorite, user_id: @user.id, product_id: @product.id)
+    favo = FactoryBot.build(:favorite, user_id: @user.id, product_id: @product.id)
+    favo.valid?
+    expect(favo.errors[:user_id]).to include("はすでに存在します")
+  end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @product = FactoryBot.create(:product)
+  end
+  # ユーザーID、商品IDがあれば有効であること
+  it "is valid with a user_id and product_id" do
+    like = Like.new(
+      user_id: @user.id,
+      product_id: @product.id,
+    )
+    expect(like).to be_valid
+  end
+  # ユーザーIDがなければ無効な状態であること
+  it "is invalid without a user_id" do
+    like = Like.new(user_id: nil)
+    like.valid?
+    expect(like.errors[:user]).to include("を入力してください")
+  end
+  # 商品IDがなければ無効な状態であること
+  it "is invalid without a product_id" do
+    like = Like.new(product_id: nil)
+    like.valid?
+    expect(like.errors[:product]).to include("を入力してください")
+  end
+  # 重複したいいねは無効な状態であること
+  it "is invalid with a duplicate likes" do
+    FactoryBot.create(:like, user_id: @user.id, product_id: @product.id)
+    like = FactoryBot.build(:like, user_id: @user.id, product_id: @product.id)
+    like.valid?
+    expect(like.errors[:user_id]).to include("はすでに存在します")
+  end
 end
