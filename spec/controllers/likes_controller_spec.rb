81de1bfd8fe63cc9430d846129cd8a71b2a7d59f
chee1989 <58983064+chee1989@users.noreply.github.com>
2020-12-30 18:59:29 +0900

    controller_test

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -8,14 +8,27 @@ RSpec.describe LikesController, type: :controller do
         @user = FactoryBot.create(:user)
         @category = FactoryBot.create(:category)
         @product = FactoryBot.create(:product, user_id: @user.id, category_id: @category.id)
+        @like = FactoryBot.create(:like, user_id: @user.id, product_id: @product.id)
         sign_in @user
       end
       # いいねできること
       # it "adds a like" do
       #   expect {
-      #           post :create, params: { user_id: @user.id, product_id: @product.id }
-      #         }.to change(Like, :count).by(1)
+      #     post :create, params: {"product_id"=>@product.id}
+      #   }.to change(Like, :count).by(1)
       # end
+      # it "deletes a like" do
+      #   expect {
+      #     binding.pry
+      #     delete :destroy, params: { id: @like.id } 
+      #   }.to change(Like, :count).by(-1)
+      # end
+      # JSON 形式でレスポンスを返すこと
+      it "responds with JSON formatted output" do
+        post :create, format: :json,
+        params: { product_id: @product.id, user_id: @user.id } 
+        expect(response.content_type).to eq "application/json"
+      end
     end
     # ゲストとして
     context "as a guest" do
